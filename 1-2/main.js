21f44cc7540b6b6f80ffc2f4114fd6d4fea2be8c
鎗田祐生 <yariyuu45@gmail.com>
2023-08-03 13:48:11 +0900

    課題提出
    第二章「変数・定数」の提出

---
+++ 1-2/main.js
@@ -0,0 +1,21 @@
+//変数・定数とは
+let name1 = "hoge";
+console.log(name1);
+
+//変数宣言時のルール
+name1 = "fuga";
+console.log(name1);
+
+let Name = "池田";
+console.log(Name + "くん");
+let number = 10;
+console.log(number * 3);
+
+console.log(`${Name}くんは${number}月生まれです`);//``(バッククォートキー　Shift+@)
+
+/*1-2課題*/
+let price = 100;
+console.log(price * 2);
+
+price = 500
+console.log(price * 2);
